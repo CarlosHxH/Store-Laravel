66f2f20b806dd760441ee7d9f0f9bb9b11606b58
CarlosHxH <carlosdaniel.prog@gmail.com>
2022-08-04 11:20:28 -0300

    category

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,58 @@
+<!-- Footer -->
+<footer class="d-flex justify-content-around bg-dark text-light">
+    <div class="container pt-4">
+        <div class="row">
+            <div class="col-md-3 col-lg-4 col-xl-3">
+                <h5>About</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <p class="mb-4">
+                    Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
+                    impression.
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto">
+                <h5>Informations</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="nav flex-column">
+                    <a href="" class="nav-link text-light">Link 1</a>
+                    <a href="" class="nav-link text-light">Link 2</a>
+                    <a href="" class="nav-link text-light">Link 3</a>
+                    <a href="" class="nav-link text-light">Link 4</a>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                <h5>Others links</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="nav flex-column">
+                    <a href="" class="nav-link text-light">Link 1</a>
+                    <a href="" class="nav-link text-light">Link 2</a>
+                    <a href="" class="nav-link text-light">Link 3</a>
+                    <a href="" class="nav-link text-light">Link 4</a>
+                </ul>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3">
+                <h5>Contact</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-home mr-2"></i> My company</li>
+                    <li><i class="fa fa-envelope mr-2"></i> email@example.com</li>
+                    <li><i class="fa fa-phone mr-2"></i> + 33 12 14 15 16</li>
+                    <li><i class="fa fa-print mr-2"></i> + 33 12 14 15 16</li>
+                </ul>
+            </div>
+            <div class="col-12 copyright mt-3">
+                <p class="float-left">
+                    <a href="#" class="text-light">Back to top</a>
+                </p>
+                <p class="text-end text-muted">created with
+                    <i class="fa fa-heart"></i> by
+                    <a href="#" class="text-link"><i class="text-light">CrxCode</i></a> | <span>v:
+                        1.0</span>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
